fb9ba4e675912f5a012599a2cceef34593051055
Zalen Nelson <zalnelso@publicisgroupe.net>
2022-11-09 19:36:01 -0500

    Starter Code

--- Code Contributions/Zalen/SolvePuzzle.cpp
+++ Code Contributions/Zalen/SolvePuzzle.cpp
@@ -8,17 +8,41 @@
 
 using namespace std;
 
+class Puzzle {
+    public: 
+    int row; 
+    int col;
+
+    Puzzle(int r, int c) {
+        row = r;
+        col = c;
+    }
+
+    Puzzle SolvePuzzle(Puzzle currentPuzzle);       
+    void PrintPuzzle();
+
+
+};
+
+Puzzle::Puzzle(int r, int c) {
+    row = r;
+    col = c;
+}
+
+Puzzle Puzzle::SolvePuzzle(Puzzle currentPuzzle) {
+
+}
+
+void Puzzle :: PrintPuzzle() {
+
+    
+}
+
 
 int main() {
-
-    // TODO: Everything
-
-
-
-
-
-
-
+    //create sample suduko puzzle
+   int puzzle[3][3] = {{1,2,3},{4,5,6},{7,8,0}};
+    
 
     return 0; 
 }
