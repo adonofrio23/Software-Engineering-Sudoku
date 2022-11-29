1315b99634762f44ea33e49db291e6bcbc40fccf
Zalen Nelson <zalnelso@publicisgroupe.net>
2022-11-29 15:56:04 -0500

    Started implementation functions in Algo classes

---
+++ Code Contributions/Zalen/Algorithms.cpp
@@ -0,0 +1,79 @@
+ #include "Algorithms.hpp"
+ #include <iostream> 
+
+using namespace std;
+// CheckVal checks if the value of the cell is valid
+bool CheckVal(Puzzle * puzzle, int row, int col) {
+    	int val = puzzle->GetCell(row, col)->GetValue();
+    	
+        if (val == 0)
+    		return true;
+    	
+        for (int i = 0; i < 9; i++) {
+    		if (i != col && puzzle->GetCell(row, i)->GetValue() == val)
+    			return false;
+    		if (i != row && puzzle->GetCell(i, col)->GetValue() == val)
+    			return false;
+    	}
+
+    	int rowStart = (row / 3) * 3;
+    	int colStart = (col / 3) * 3;
+
+    	for (int i = rowStart; i < rowStart + 3; i++) {
+    		for (int j = colStart; j < colStart + 3; j++) {
+    			if (i != row && j != col && puzzle->GetCell(i, j)->GetValue() == val) {
+    				return false;
+                }
+    		}
+    	}
+    	return true;
+    }
+
+
+bool SolveBruteForce(Puzzle* puzzle) {
+    for (int i = 0; i < 9; i++) {
+    	for (int j = 0; j < 9; j++) {
+    		if (puzzle->GetCell(i, j)->GetValue() == 0) {
+    			for (int k = 1; k <= 9; k++) {
+    				puzzle->GetCell(i, j)->SetValue(k);
+    				if (CheckVal(puzzle, i, j) && SolveBruteForce(puzzle))        
+    					return true;
+    				puzzle->GetCell(i, j)->SetValue(0);
+    			}
+    		  return false;
+    	    }
+    	}
+    }
+    	return true;
+}
+
+
+// CheckPuzzle checks if the puzzle is correct
+// https://www.geeksforgeeks.org/check-if-given-sudoku-board-configuration-is-valid-or-not/
+
+bool CheckPuzzle(Puzzle* puzzle) {
+    for (int i = 0; i < 9; i++) {
+    	for (int j = 0; j < 9; j++) {
+    		if (!CheckVal(puzzle, i, j))
+    			return false;
+    	}
+    }
+    return true;
+}
+
+void PrintPuzzle(Puzzle* puzzle) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            cout << puzzle->GetCell(i, j)->GetValue() << ',';
+        }
+        cout << "\n";
+    }
+}
+
+void setPuzzle(Puzzle* puzzle, int* puzzleArray) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            puzzle->GetCell(i, j)->SetValue(puzzleArray[i * 9 + j]);
+        }
+    }
+}
