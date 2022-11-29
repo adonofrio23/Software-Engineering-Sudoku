9cc28ca72fcdf32c73f4f1c8052ab4064fad5401
Sarah Chai <sarahchai93@gmail.com>
2022-11-29 17:29:15 -0500

    Undo() UndoUntilCorrect() code done

---
+++ Code Contributions/Sarah/GameEngine.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "GameEngine.h"
+
+using namespace std;
+
+
+void GameEngine::Undo() {
+    // pop the latest entry in history
+    Entry entry = history.PopHistory();
+
+    if (entry == NULL) {
+        exit; 
+    } else {
+        // set the cell back to the original cell
+        puzzle.SetCell(entry.GetOrigCell());
+    }
+}
+
+void GameEngine::UndoUntilCorrect() {
+    // pop the latest entry in history 
+    Entry tempEntry = history.PopHistory();
+
+    // if isCorrect() == false then go into while loop
+    while (!tempEntry.isCorrect()) {
+        // set the cell back to the original cell
+        puzzle.SetCell(tempEntry.GetOrigCell());
+        // pop the SetCell of the previous line - check SetCell implementation in Puzzle
+        history.PopHistory();
+        // pop another entry from history to check 
+        tempEntry = history.PopHistory();
+    }
+
+    // if isCorrect() == true then it will push back the history and exit
+    history.PushHistory(tempEntry);
+}
+
