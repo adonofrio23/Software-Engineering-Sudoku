2eb9a7724f6ea67d836d087deeba1a5cfb698f5d
adonofrio10 <70344654+adonofrio10@users.noreply.github.com>
2022-12-01 10:57:38 -0500

    Tested cell code

--- Code Contributions/Anthony/cell.cpp
+++ Code Contributions/Anthony/cell.cpp
@@ -1,90 +1,115 @@
-using namespace std;
 #include <iostream>
 #include <list>
+#include "cell.h"
+using namespace std;
 
-// Cell Object
-class Cell {
-public:
-    // Data  Members
-    int value;
-    int size;
-    int notes [16];
-    bool hardWired;
-    int solution;
-    int row;
-    int col;
+// Member Functions()
+int Cell::getSolution() 
+{
+    return solution;
+}
 
-    // Member Functions()
-    int getSolution() 
-    {
-        return solution;
-    }
+void Cell::setSolution(int answer)
+{
+    solution = answer;
+}
 
-    void setSolution(int answer)
-    {
-        solution = answer;
-    }
+int Cell::getValue()
+{
+    return value;
+}
 
-    int getValue()
-    {
-        return value;
-    }
+void Cell::setValue(int val)
+{
+    value = val;
+}
 
-    void setValue(int val)
-    {
-        value = val;
-    }
+bool Cell::getHardWired()
+{
+    if (hardWired == true)
+        return true;
 
-    bool isHardWired()
-    {
-        if (hardWired == true)
-            return true;
+    return false;
+}
 
-        return false;
-    }
+void Cell::setHardWired(bool hardWired)
+{
+    hardWired = true;
+}
 
-    void setHardWired(bool hardWired)
-    {
-        hardWired = true;
-    }
+int * Cell::getNotes()
+{
+    return notes;
+}
 
-    void setSize(int s)
-    {
-        size = s;
-    }
+// For testing purposes only
+void Cell::setNotes()
+{
+    for(int i = 0; i < 16; i++)
+        notes[i] = 0;
+}
 
-    int getSize() 
-    {
-        return size;
-    }
+void Cell::setNotesValue(int value)
+{
+    if (notes[value-1] == 0)
+        notes[value-1] = value;
+    else
+        notes[value-1] = 0;
+}
 
-    void setNotes(int value)
-    {
-        if (notes[value-1] = 0)
-            notes[value-1] = value;
-        else
-            notes[value-1] = 0;
-    }
+// For testing purposes only
+void Cell::printNotes()
+{
+    for(int i = 0; i < 16; i++)
+        cout << notes[i] << " ";
+    cout << endl;
+}
 
-    int * getNotes()
-    {
-        return notes;
-    }
+int Cell::getRow()
+{
+    return row;
+}
 
-    int getRow()
-    {
-        return row;
-    }
+void Cell::setRow(int r)
+{
+    row = r;
+}
 
-    int getCol()
-    {
-        return col;
-    }
+int Cell::getCol()
+{
+    return col;
+}
 
-};
+void Cell::setCol(int c)
+{
+    col = c;
+}
 
 // Testing Cell Object 
 int main()
 {
+    Cell cellTest;
+   
+    cellTest.setNotes();
+    cellTest.setNotesValue(5);
+    cellTest.setNotesValue(1);
+    cellTest.setNotesValue(15);
+
+
+    cellTest.setSolution(6);
+    cellTest.setValue(5);
+    cellTest.setHardWired(true);
+    cellTest.setRow(4);
+    cellTest.setCol(3);
+
+
+    cout << cellTest.getSolution() << endl;
+    cout << cellTest.getValue() << endl;
+    cout << cellTest.getHardWired() << endl;
+    cout << cellTest.getRow() << endl;
+    cout << cellTest.getCol() << endl;
+    cellTest.printNotes();
+
+    
     
 }
--- Code Contributions/Anthony/cell.h
+++ Code Contributions/Anthony/cell.h
@@ -1,37 +1,33 @@
 class Cell
 {
     public:
-    // Data Members
+    // Member Functions()
+    int getSolution();
+    void setSolution(int answer);
+    
+    int getValue();
+    void setValue(int val);
+   
+    bool getHardWired();
+    void setHardWired(bool hardWired);
+    
+    int * getNotes();
+    void setNotes();
+    void setNotesValue(int value);
+    void printNotes();
+
+    int getRow();
+    void setRow(int r);
+
+    int getCol();
+    void setCol(int c);
+
+    private:
+    // Data  Members
     int value;
-    int size;
-    list<int> notes;
+    int notes [16];
     bool hardWired;
     int solution;
     int row;
     int col;
-    
-     // Member Functions()
-    int getSolution() 
-    void setSolution(int answer)
-   
-    int getValue()
-    void setValue(int val)
-   
-
-    bool isHardWired()
-    void setHardWired(bool hardWired)
-  
-    void setSize(int s)
-    int getSize() 
-
-    void setNotes()
-    list<int> getNotes()
-
-    int getRow()
-    int getCol()
-   
-    // Constructor
-    // Add Notes and notes size to constructor
-    Cell(int r, int c)
-    
 };
