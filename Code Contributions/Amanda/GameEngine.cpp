9617428af3eca7d6f4d50802b75843e9aa11d9a7
anmartin95 <amartin95@fordham.edu>
2022-12-04 19:42:15 -0500

    updated GameEngine cpp

--- Code Contributions/Amanda/GameEngine.cpp
+++ Code Contributions/Amanda/GameEngine.cpp
@@ -6,17 +6,25 @@
 
 using namespace std;
 
+// sets currentValue member variable with val parameter
 void GameEngine::SetCurrentValue(int val)
 {
     currentValue = val;
 }
 
+// returns currentValue member variable
 int GameEngine::GetCurrentValue()
 {
     return currentValue;
 }
 
-// bool returns based off of
+/*  param row: row of the given cell
+    param col: column of the given cell
+    gets cell at given row and column, sets the value of the cell in the puzzle
+    creates a new entry with the cell's updated information and pushes it to the stack
+    returns true if value is successfully set in the given cell
+    returns false if value cannot be set in the given cell
+*/
 bool GameEngine::SetValue(int row, int col)
 {
     Cell thisCellOrig = puzzle.GetCell(row, col);
@@ -37,6 +45,12 @@ bool GameEngine::SetValue(int row, int col)
     return false;
 }
 
+/*  param row: row number of given cell
+    param col: column number of given cell
+    uses puzzle member object to create copy of cell object at location (row, col)
+    and cell member function SetNotes to update cell variables
+    returns pointer to int array of notes of the given cell
+*/
 int* GameEngine::SetNote(int row, int col)
 {
     // based off 
