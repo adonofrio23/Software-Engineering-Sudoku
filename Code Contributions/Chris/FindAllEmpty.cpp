9ab199b168fa552cfccbded849e2a5d10e6d5a83
cschultz11 <91289400+cschultz11@users.noreply.github.com>
2022-12-14 17:14:30 -0500

    Updated code snippet for Cell pointers

--- Code Contributions/Chris/FindAllEmpty.cpp
+++ Code Contributions/Chris/FindAllEmpty.cpp
@@ -4,15 +4,15 @@
 
 //Code Stub for the FindAllEmpty Algorithm
 //NOTE: No longer uses array, instead vector for full functionality
-public vector<Cell> Algorithms::FindAllEmpty(Puzzle* puzzle)
+//Uses Cell pointers due to Cell as a pointer in main, prevents errors
+public vector<Cell*> Algorithms::FindAllEmpty(Puzzle* puzzle)
 {
 	//Variables
 	//emptyList: Returned Value; List of type Cell that contains found empty values
 	//size: Used as iterator; Puzzle size used for both row and column limits because of square puzzle
 	//count: Used as iterator in emptyList; Sets location in emptyList, add for each found empty value
-	vector<Cell> emptyList;
+	vector<Cell*> emptyList;
 	int size = puzzle->GetSize();
-	cout << puzzle->GetSize() << endl;
 	//For loop for rows within the puzzle, using size
 	for (int row = 0; row < size; row++)
 	{
@@ -22,7 +22,7 @@ public vector<Cell> Algorithms::FindAllEmpty(Puzzle* puzzle)
 			//If statement that does the following:
 			//Checks if the value of the cell is 0, this means it is empty
 			//If the cell is empty...
-			if (puzzle->GetCell(row, col).GetValue() == 0)
+			if (puzzle->GetCell(row, col)->GetValue() == 0)
 			{
 				//Add the Cell (not Cell value) to emptyList at point count
 				//Then increment count for the next position in emptyList
