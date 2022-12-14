acbdc7c13ab7e8348c0ea55dabe7baa24f639dc9
Zalen Nelson <zalnelso@publicisgroupe.net>
2022-12-14 15:55:34 -0500

    Minor changes

--- Code Contributions/Zalen/Puzzle.cpp
+++ Code Contributions/Zalen/Puzzle.cpp
@@ -42,7 +42,7 @@ using namespace std;
     void LoadPuzzle(Puzzle* puzzle, int* puzzleArray) {
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
-            puzzle->GetCell(i, j)->SetValue(puzzleArray[i * 9 + j]);
+            puzzle->GetCell(i, j).SetValue(puzzleArray[i * 9 + j]);
 		}
 	}
 }
