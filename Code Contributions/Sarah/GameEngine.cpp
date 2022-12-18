00eb6866f825341e454cb5ad704ef483fb6d475f
Sarah Chai <sarahchai93@gmail.com>
2022-12-18 01:21:07 -0500

    Updated UnitTesting and non code documents

--- Code Contributions/Sarah/GameEngine.cpp
+++ Code Contributions/Sarah/GameEngine.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include "./../Dylon/Puzzle.h"
+#include "./../Alexis/History.h"
+#include "./../Anthony/Cell.h"
 #include "../Amanda/GameEngine.h"
 using namespace std;
 
@@ -46,10 +49,129 @@ void GameEngine::UndoUntilCorrect() {
     }
 }
 
+// // Unit test for Undo and UndoUntilCorrect
+// int main() {
+//     History history; 
+//     Puzzle puzzle;
+    
+//     cout<<"------------Unit Testing-----------"<<endl;
 
-// void GameEngine::SetUp(Puzzle* gamePuzzle, History* gameHistory) {
-//     puzzle = gamePuzzle;
-//     history = gameHistory;
-//     currentValue = 0;
-//     notesMode = false;
+//     cout<<"** Testing Undo() with empty history **"<<endl;
+//     cout<<"Input: None"<<endl;
+//     // Unable to call Undo without creating game engine
+//     // so just copied the code of Undo()
+//     if (!history.IsHistoryEmpty()) {
+//         Entry* entry = history.TopOfStack();
+//         Cell* origCell = entry->GetOrigCell();
+//         puzzle.SetCell(*origCell);
+//         history.PopHistory();
+//     }
+
+//     cout<<"Output: No crash and no change"<<endl;
+//     cout<<endl;
+
+//     cout<<"** Testing Undo() with not empty history **"<<endl;
+
+//     // pushing two entries to history
+//     cout<<"Input: Two enties in history stack (value 1 - value 2)"<<endl;
+//     Cell origCell1;
+//     Cell newCell1;
+//     bool stat1 = false;
+    
+//     origCell1.SetValue(1);
+//     Entry entry1(&origCell1, &newCell1, stat1);
+//     history.PushHistory(&entry1);
+
+//     Cell origCell2;
+//     Cell newCell2;
+//     bool stat2 = false;
+
+//     origCell2.SetValue(2);
+//     Entry entry2(&origCell2, &newCell2, stat2);
+//     history.PushHistory(&entry2);
+
+//     // Undoing and printing values
+//     cout<<"Output: ";
+//     if (!history.IsHistoryEmpty()) {
+//         Entry* entry = history.TopOfStack();
+//         Cell* origCell = entry->GetOrigCell();
+//         puzzle.SetCell(*origCell);
+//         Entry* popped = history.PopHistory();
+//         cout<<"popped value "<<popped->GetOrigCell()->GetValue()<<", ";
+//     }
+
+//     cout<<"value from history stack entry "<<history.TopOfStack()->GetOrigCell()->GetValue()<<endl;
+//     cout<<endl;
+
+//     // Undoing one more time for next test
+//     if (!history.IsHistoryEmpty()) {
+//         Entry* entry = history.TopOfStack();
+//         Cell* origCell = entry->GetOrigCell();
+//         puzzle.SetCell(*origCell);
+//         Entry* popped = history.PopHistory();
+//     }
+
+
+//     cout<<"** Testing UndoUntilCorrect() with empty history **"<<endl;
+//     cout<<"Input: None"<<endl;
+//     // UndoUntilCorrect()
+//     if (!history.IsHistoryEmpty()) {
+//         Entry* tempEntry = history.TopOfStack();
+//         while (!tempEntry->IsCorrect()) {
+//             Cell* origCell = tempEntry->GetOrigCell();
+//             puzzle.SetCell(*origCell);
+//             history.PopHistory();
+//             if (history.IsHistoryEmpty()) {
+//                 break;
+//             } else {
+//                 tempEntry = history.TopOfStack();
+//             }
+//         }
+//     }
+//     cout<<"Output: No crash and no change"<<endl;
+//     cout<<endl;
+
+//     cout<<"** Testing UndoUntilCorrect() with not empty history **"<<endl;
+
+//     // pushing 3 entryes with one true and two false 
+//     cout<<"Input: ";
+//     Cell origCell3;
+//     Cell newCell3;
+//     bool stat3 = true;
+    
+//     origCell3.SetValue(1);
+//     Entry entry3(&origCell3, &newCell3, stat3);
+//     history.PushHistory(&entry3);
+//     cout<<"value 1 true entry, ";
+
+//     origCell1.SetValue(2);
+//     history.PushHistory(&entry1);
+//     cout<<"value 2 false entry, ";
+
+//     origCell2.SetValue(3);
+//     history.PushHistory(&entry2);
+//     cout<<"value 3 false entry pushed"<<endl;
+
+//     // Undoing until correct with printing values
+//     cout<<"Output: ";
+//     if (!history.IsHistoryEmpty()) {
+//         Entry* tempEntry = history.TopOfStack();
+//         while (!tempEntry->IsCorrect()) {
+//             Cell* origCell = tempEntry->GetOrigCell();
+//             puzzle.SetCell(*origCell);
+//             Entry* popped = history.PopHistory();
+
+//             cout<<popped->GetOrigCell()->GetValue()<<" popped "<<endl;
+//             if (history.IsHistoryEmpty()) {
+//                 break;
+//             } else {
+//                 tempEntry = history.TopOfStack();
+//             }
+//         }
+//     }
+
+//     cout<<"value from history stack entry "<<history.TopOfStack()->GetOrigCell()->GetValue()<<endl;
+//     cout<<endl;
+
+//     return 0;
 // }
